c03119d5577431d2cdad12406d2bdb25d10c7431
SamWuu <iamkeason@gmail.com>
2017-01-06 14:08:15 +0800

    route modify

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome/intro' => 'welcome#intro'
-  root 'welcome/intro'
 end
