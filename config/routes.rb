0b4fb40bf9b60d767ecea1e1faf986d7a4b7c73f
samwuu <iamkeason@gmail.com>
2017-01-06 05:54:09 +0000

    about page img append

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/about'
+  get 'about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'welcome/intro' => 'welcome#intro'
-  root 'welcome#about'
+  get 'intro' => 'welcome#intro'
+  root 'intro'
 end
