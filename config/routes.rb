a5408191d07a82f27109cff5f9df6aaeab707c55
SamWuu <iamkeason@gmail.com>
2017-01-06 14:05:52 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'about'
+  get 'welcome/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'intro' => 'welcome#intro'
+  get 'welcome/intro' => 'welcome#intro'
   root 'intro'
 end
