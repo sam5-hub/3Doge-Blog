488d7b68fd15d2eb5726355533edfa0e26cdd154
samwuu <iamkeason@gmail.com>
2017-01-10 02:47:13 +0000

    install bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :articles
   get 'welcome/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome/intro' => 'welcome#intro'
-  root 'welcome#intro'
+  root 'articles#index'
 
 end
