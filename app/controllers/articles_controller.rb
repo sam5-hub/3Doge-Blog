14d9cbab7ea1d531c781f13000cca9fdf2912747
samwuu <iamkeason@gmail.com>
2017-01-13 02:51:33 +0000

    add errors page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,40 +25,42 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
+    if @article.save
+      flash[:success] = "Article was successfully created."
+      redirect_to article_path(@article)
+    else
+      flash[:danger] = "Creation of article was failed"
+      render :new
+    end  
   end
 
   # PATCH/PUT /articles/1
   # PATCH/PUT /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
+    if @article.save
+      flash[:success] = "Article was successfully updated."
+      redirect_to article_path(@article)
+    else
+      flash[:danger] = "updation of article was failed"
+      render :edit
+    end 
   end
-
+ 
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
-    @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    if @article.destroy
+      flash[:danger] = "Article was successfully deleted."
+      redirect_to articles_path
+    else
+      flash[:warning] = "deletion of article was failed"
+      render articles_path
+    end 
+    
+    # respond_to do |format|
+    #   format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
   end
 
   private
